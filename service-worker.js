86c31433c37547de6bb583203bbe1872c57b6083
Yury Savin <savinyurii@gmail.com>
2018-12-13 01:26:28 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5e21adbf4dfc20350aca4a537f84a9cf.js"
+  "/precache-manifest.6ac4144a251c1b460ea3b3f6c3bea77d.js"
 );
 
 workbox.clientsClaim();
